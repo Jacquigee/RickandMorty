37389e0d96bc666e89346e308481eca3a56a84d6
Jacquigee <gitaujaquiline@gmail.com>
2026-03-30 11:02:37 +0300

    Minor changes

--- app/src/main/java/com/jacqui/rickandmorty/view/CharacterScreen.kt
+++ app/src/main/java/com/jacqui/rickandmorty/view/CharacterScreen.kt
@@ -24,6 +24,7 @@ import androidx.paging.compose.itemKey
 import com.jacqui.rickandmorty.R
 import com.jacqui.rickandmorty.data.domain.CharacterResultDomain
 import com.jacqui.rickandmorty.view.component.CharacterListItem
+import com.jacqui.rickandmorty.view.ui.theme.RickAndMortyTheme
 import com.jacqui.rickandmorty.view.viewmodel.CharacterViewModel
 import org.koin.compose.viewmodel.koinViewModel
 
--- app/src/main/java/com/jacqui/rickandmorty/view/component/ListItem.kt
+++ app/src/main/java/com/jacqui/rickandmorty/view/component/ListItem.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -46,7 +47,7 @@ fun CharacterListItem(
     Card(
         modifier = modifier
             .padding(horizontal = 16.dp, vertical = 8.dp)
-            .fillMaxWidth()
+            .fillMaxSize()
             .clickable{
                 onCharacterClicked()
             },
